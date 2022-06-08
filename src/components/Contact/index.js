4159584e617a8ffe20188e1a12e0049aec5db230
David Samuelson <linksn.1fan@gmail.com>
2022-06-08 12:50:44 -0600

    added contact form styling

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -22,19 +22,22 @@ function ContactForm() {
       }
 
     return (
-        <section>
+        <section className="form-cont">
           <h1>Contact me</h1>
           <form id="contact-form" onSubmit={handleSubmit}>
             <div>
                 <label htmlFor="name">Name:</label>
+                <br />
                 <input type="text" name="name" defaultValue={name} onBlur={handleChange}/>
             </div>
             <div>
                 <label htmlFor="email">Email address:</label>
+                <br />
                 <input type="email" name="email" defaultValue={email} onBlur={handleChange}/>
             </div>
             <div>
                 <label htmlFor="message">Message:</label>
+                <br />
                 <textarea name="message" rows="5" defaultValue={message} onBlur={handleChange}/>
             </div>
             {errorMessage && (<div>
