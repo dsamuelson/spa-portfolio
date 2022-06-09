75bcd4bbe44d54387b4d8b06f560e9026d7b60f2
David Samuelson <linksn.1fan@gmail.com>
2022-06-09 15:32:48 -0600

    added resume page and download pdf

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -3,6 +3,7 @@ import React from "react";
 function About() {
     return (
         <section className="about-cont">
+            <img src={require("../../assets/images/profile-pic.jpg")} />
             <h2> About </h2>
             <p>
                 I love web development, and to an extent I think that is the most important thing about me. When you find someone who's passion is in the line of work you are offering that person will be the best asset to your team and company in general. I started programming with visual basic back when I was 12. My father was in computer development long before they were even out for public consumption. He taught me the basics of creating an application for computers at that time and was surprised by my progress 4 days later when I was creating dynamic elements and working with array methods. Life happened and I kind of lost that passion for a while and when I started up again I realized this was indeed my calling. I need to be a developer! It's in my heart and soul to create awesome pages and applications that people can use and enjoy and can benefit the world.
